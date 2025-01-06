77fdc353713f5b37b933646c2982e7e7f5c10760
Zaynab-ahmad <zaynab.g.ahmad@gmail.com>
2025-01-06 22:46:23 +0300

    Categories and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import NavBar from './Components/NavBar/NavBar'
 import Home from './Pages/Home/Home'
 import MoviesAndShows from './Pages/MoviesAndShows/MoviesAndShows'
@@ -10,7 +10,7 @@ import MoviesPageOpen from './Pages/MoviesPageOpen/MoviesPageOpen'
 import ShowsPageOpen from './Pages/ShowsPageOpen/ShowsPageOpen'
 import Subscription from './Pages/Subscription/Subscription'
 import Support from './Pages/Support/Support'
-import MainFooter from './Components/MainFooter/MainFooter';
+import MainFooter from './Components/MainFooter/MainFooter'
 function App() {
  
   return (

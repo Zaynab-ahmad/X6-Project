d00d91ad0843d0acb32f2b3569d280f033a3ae45
Emad Maqsosa <137618483+EmadMaqsosa@users.noreply.github.com>
2025-01-08 19:30:23 +0300

    Main

--- src/Components/Cards/PlanCard/PlanCard.jsx
+++ src/Components/Cards/PlanCard/PlanCard.jsx
@@ -1,9 +1,28 @@
-import './PlanCard.css'
+import "./PlanCard.css";
 
-export default function PlanCard() {
-  return (
-    <div>
-      
-    </div>
-  )
+function PlanCards({ plans, isMonthly }) {
+  const displayedPlans = plans.map((plan, index) => {
+    const price = isMonthly ? plan.monthlyPrice : plan.yearlyPrice;
+    const period = isMonthly ? "/month" : "/year";
+
+    return (
+      <div className="plan" key={index}>
+        <h2>{plan.name}</h2>
+        <p>{plan.description}</p>
+        <div className="price">
+          ${price}
+          <span>{period}</span>
+        </div>
+        <div className="button-container">
+          <button className="trial">Start Free Trial</button>
+          <button>Choose Plan</button>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="plans">{displayedPlans}</div>;
+
 }
+
+export default PlanCards;
--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,9 +1,65 @@
-import './Pricing.css'
+import { useState } from "react";
+import "./Pricing.css"
+import PlanCards from "/X6-Project/src/Components/Cards/PlanCard/PlanCard";
 
-export default function Pricing() {
+function Pricing() {
+  const [isMonthly, setIsMonthly] = useState(true);
+
+  const plansData = [
+    {
+      name: "Basic Plan",
+      description:
+        "Enjoy an extensive library of movies and shows, featuring a range of content, including recently released titles.",
+      monthlyPrice: 9.99,
+      yearlyPrice: 99.99
+    },
+    {
+      name: "Standard Plan",
+      description:
+        "Access to a wider selection of movies and shows, including most new releases and exclusive content",
+      monthlyPrice: 12.99,
+      yearlyPrice: 129.99
+    },
+    {
+      name: "Premium Plan",
+      description:
+        "Access to a widest selection of movies and shows, including all new releases and Offline Viewing",
+      monthlyPrice: 14.99,
+      yearlyPrice: 149.99
+    }
+  ];
+
+  const handleToggle = (monthly) => {
+    setIsMonthly(monthly);
+  };
+
   return (
-    <div>
-      Pricing
+    <div className="container">
+      <div className="part1">
+        <div className="part2">
+          <h1>Choose the plan that is right for you</h1>
+          <p className="pa">
+            Join StreamVibe and select from our flexible subscription options
+            tailored to suit your viewing pref____-_______-__erences. Get ready
+            for non-stop entertainment!
+          </p>
+        </div>
+        <div className="toggle-buttons">
+          <button
+            className={`toggle ${isMonthly ? "active" : ""}`}
+            onClick={() => handleToggle(true)}>
+            Monthly
+          </button>
+          <button
+            className={`toggle ${!isMonthly ? "active" : ""}`}
+            onClick={() => handleToggle(false)}>
+            Yearly
+          </button>
+        </div>
+      </div>
+      <PlanCards plans={plansData} isMonthly={isMonthly} />
     </div>
-  )
+  );
 }
+
+export default Pricing;

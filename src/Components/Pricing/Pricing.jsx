30b268200842706ca77f43577038eb9b9a92225d
Emad Maqsosa <137618483+EmadMaqsosa@users.noreply.github.com>
2025-01-14 14:03:04 +0300

    update

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -41,8 +41,8 @@ function Pricing() {
   };
 
   return (
-    <Container className="mt-4 d-flex flex-column">
-      <Row>
+    <Container className="mt-4 xContainer customMargin">
+      <Row className="w-100 p-4">
         <Col
           md={10}
           lg={11}
@@ -50,18 +50,18 @@ function Pricing() {
           className="d-flex justify-content-start flex-column">
           <Row className="d-flex div-flex">
             <Col md={10}>
-              <h1>Choose the plan that is right for you</h1>
-              <p className="pa">
+              <h1 className="main-title">Choose the plan that is right for you</h1>
+              <p className="main-paragraph">
                 Join StreamVibe and select from our flexible subscription
                 options tailored to suit your viewing preferences. Get ready for
                 non-stop entertainment!
               </p>
             </Col>
             <Col xs={6} md={2} className="d-flex align-items-center">
-              <ButtonGroup toggle className="toggle-buttons p-2">
+              <ButtonGroup toggle className="toggle-btn p-2">
                 <ToggleButton
                   type="radio"
-                  variant="text-center p-3 toggle2"
+                  variant="text-center p-3 toggle"
                   name="radio"
                   value={true}
                   checked={isMonthly}
@@ -70,7 +70,7 @@ function Pricing() {
                 </ToggleButton>
                 <ToggleButton
                   type="radio"
-                  variant="text-center p-3 toggle2"
+                  variant="text-center p-3 toggle"
                   name="radio"
                   value={false}
                   checked={!isMonthly}
@@ -83,7 +83,7 @@ function Pricing() {
         </Col>
       </Row>
       <Container className="mt-3 d-flex justify-content-center align-items-center">
-          <PlanCards plans={plansData} isMonthly={isMonthly} />
+        <PlanCards plans={plansData} isMonthly={isMonthly} />
       </Container>
     </Container>
   );

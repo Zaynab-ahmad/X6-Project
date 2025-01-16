8b5eace4ed2a098f3fae11b2921454322b0a5744
SibaAli609 <sibagamilali@gmail.com>
2025-01-17 02:24:23 +0600

    update

--- src/Components/MoviesPageCast/MoviesPageCast.jsx
+++ src/Components/MoviesPageCast/MoviesPageCast.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MoviesPageCast() {
   return (
     <div>
-      Cast
+     
     </div>
   )
 }
--- src/Components/MoviesPageDes/MoviesPageDes.jsx
+++ src/Components/MoviesPageDes/MoviesPageDes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MoviesPageDes() {
   return (
     <div>
-      Description
+      
     </div>
   )
 }

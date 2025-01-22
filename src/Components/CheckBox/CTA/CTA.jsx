3a77d801a807ef5f3bdc8093cab105b88f59f04f
KaramReqmany <karamreqmany546@gmail.com>
2025-01-22 20:44:05 +0300

    update

---
+++ src/Components/CheckBox/CTA/CTA.jsx
@@ -0,0 +1,52 @@
+import Button from "../Buttons/Button";
+import "./CTA.css";
+import HomeParagraph from "../SharedTitlesAndParagraph/HomeParagraph/HomeParagraph";
+import HomeMoviesAndShowsTitle from "../SharedTitlesAndParagraph/HomeMoviesAndShows/HomeMoviesAndShowsTitle";
+
+export default function CTA() {
+  return (
+    <div className="xContainer customMargin">
+      <div className="ctaSA">
+        {/* Content Section */}
+        <div className="ctaContentSA d-flex flex-column flex-xl-row align-items-center">
+          <div className="ctaTextSection d-flex flex-column align-items-center align-items-xl-start text-center text-xl-start">
+            <div className="d-none d-sm-block">
+            <HomeMoviesAndShowsTitle
+              title= "Start your free trial today!"
+              titlePadding="home"
+              fontSizeType="type1"
+              lineHeightType="type1"
+            />
+            </div>
+            <div className="d-sm-none">
+            <HomeMoviesAndShowsTitle
+              title={
+                <>
+                  Start your free trial
+                  <br />
+                  today!
+                </>
+              }
+              titlePadding="home"
+              fontSizeType="type1"
+              lineHeightType="type1"
+            />
+            </div>
+
+            <HomeParagraph
+              paragraph="This is a clear and concise call to action that encourages users
+              to sign up for a free trial of StreamVibe."
+            />
+          </div>
+          <Button
+            text="Start a Free Trial"
+            isFullWidth={false}
+            isSmallerBorderRaduis={true}
+            isBlack={false}
+            paddingType="type2"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Components/SupportForm/SupportForm.jsx
+++ src/Components/SupportForm/SupportForm.jsx
@@ -1,9 +1,155 @@
 import './SupportForm.css';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import flag1 from '../../assets/Icons/flag.png';
+import flag2 from '../../assets/Icons/saudi-arabia-flag-icon.png';
+import flag3 from '../../assets/Icons/united-states-flag-icon.png';
+import flag4 from '../../assets/Icons/germany-flag-icon.png';
+import flag5 from '../../assets/Icons/france-flag-icon.png';
+import Button from '../Buttons/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 export default function SupportForm() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+    isChecked: false,
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+
+  
+  const handleCheckboxChange = () => {
+    setFormData({ ...formData, isChecked: !formData.isChecked });
+  };
+
+  const handleSendMessage = () => {
+
+    console.log("Form Data Submitted:", formData);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      message: "",
+      isChecked: false,
+    });
+  };
+
+
   return (
-    <div className="supportForm">
-    
-    </div>
+    <Container className='SupportForm'>
+      <Row >
+        <Col md={6} sm={12}>
+          <div className='inputContainer bottom-margin'>
+            <label className='inputLabel' >First Name</label>
+            <input
+              className="SupportFormInput"
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              placeholder="Enter First Name"
+            />
+
+
+          </div>
+        </Col>
+        <Col md={6} sm={12}>
+          <div className='inputContainer bottom-margin'>
+            <label className='inputLabel' >Last Name</label>
+            <input
+              className="SupportFormInput"
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              placeholder="Enter Last Name"
+            />
+
+
+          </div>
+        </Col>
+      </Row>
+      <Row >
+        <Col md={6} sm={12}>
+          <div className='inputContainer bottom-margin'>
+            <label className='inputLabel' >Email</label>
+            <input
+              className="SupportFormInput"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Enter your Email"
+            />
+
+
+          </div>
+        </Col>
+        <Col md={6} sm={12}>
+          <div className='inputContainer bottom-margin'>
+            <label className='inputLabel' >Phone Number</label>
+            <div className='inputContainer'>
+              <input
+                className="SupportFormInput"
+                type="tel"
+                name="phone"
+                value={formData.phone}
+                onChange={handleInputChange}
+                placeholder="Enter Phone Number"
+              />
+
+
+            </div>
+
+          </div>
+        </Col>
+      </Row>
+      <Row >
+        <Col md={12} sm={12}>
+          <div className='inputContainer bottom-margin'>
+            <label className='inputLabel' >Message</label>
+            <textarea
+              className="SupportFormArea"
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              placeholder="Enter your Message"
+            />
+
+          </div>
+        </Col>
+
+      </Row>
+      <Row className='d-flex  justify-between align-items-center'>
+        <Col md={9} sm={12}>
+          <div className='CheckBoxArea  '>
+            <label className="custom-checkbox">
+              <input
+                type="checkbox"
+                checked={formData.isChecked}
+                onChange={handleCheckboxChange}
+              />
+              <span className="checkbox-mark"></span>
+            </label>
+
+            <p>I agree with Terms of Use and Privacy Policy</p>
+
+          </div>
+        </Col>
+        <Col md={3} sm={12}>
+          <Button onClick={handleSendMessage} text="Send Message" isFullWidth={true} paddingType="type2" isSmallerBorderRadius={true} isBlack={false} />
+        </Col>
+      </Row>
+
+    </Container>
   );
-}
+};
